360dbea808cff96cdd6f6bdaedcec04065e77c06
Artemy Gibson <artemygibson@gmail.com>
2023-06-05 10:22:15 -0700

    Change return value of most_rides and best_record methods

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -11,15 +11,25 @@ class BikeClub
   end
 
   def most_rides
-    @bikers.max_by do |biker|
+    return nil if @bikers.all? { |biker| biker.rides.empty? }
+    sorted_bikers = @bikers.group_by do |biker|
       biker.rides.values.flatten.length
     end
+    most_rides = sorted_bikers.keys.max
+    record = Hash.new
+    record[sorted_bikers[most_rides]] = most_rides
+    record
   end
 
   def best_record(ride)
+    return nil if @bikers.none? { |biker| biker.rides.include?(ride) }
     bikers_with_ride = @bikers.select { |biker| biker.rides.include?(ride) }
-      bikers_with_ride.min_by do |biker|
+      bikers_ranked = bikers_with_ride.group_by do |biker|
         biker.rides[ride].sort[0]
       end
+    best_time = bikers_ranked.keys.min
+    record = Hash.new
+    record[bikers_ranked[best_time]] = best_time
+    record
   end
 end
